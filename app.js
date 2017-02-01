7ea81f77d7faac157a5f800cb8ffb1467ef84732
YILI-HOME\yili <denverdino@gmail.com>
2017-02-02 00:00:43 +0800

    Add signal handler

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const koa = require("koa");
 const http = require('http');
 const app = new koa();
 
+process
+  .on('SIGTERM', shutdown('SIGTERM'))
+  .on('SIGINT', shutdown('SIGINT'))
+  .on('uncaughtException', shutdown('uncaughtException'));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`Receive ${ signal }, exiting ...`);
+    if (err) console.error(err.stack || err);
+    process.exit(err ? 1 : 0);
+  };
+}
+
 app.use(async (ctx, next) => {
   const start = new Date();
   await next();
@@ -13,6 +26,8 @@ app.use(async (ctx) => {
     ctx.body = "hello world"
 });
 
+
 http
     .createServer(app.callback())
     .listen(3000, () => console.log('listen on port 3000'));
+
