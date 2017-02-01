7ef9b268ae233a9427397fcafd2d32452b0b76a0
YILI-HOME\yili <denverdino@gmail.com>
2017-02-01 23:29:47 +0800

    Add timer middleware

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const koa = require("koa");
 const http = require('http');
 const app = new koa();
 
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
 app.use(async (ctx) => {
     ctx.body = "hello world"
 });
